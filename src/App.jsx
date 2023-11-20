8ec77ccba70d08ecf36e367de5b68035b9d994db
etchepared <etcheparede@gmail.com>
2023-11-20 15:07:22 -0300

    feat: add post NewTask

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import { NewTask } from "./components/NewTask";
 import { TasksList } from "./components/TasksList";
 
 function App() {
   return (
     <>
+      <NewTask />
+      <hr />
       <TasksList />
     </>
   );
