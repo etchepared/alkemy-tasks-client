bc2ef3efd6b40d4239006542d673180b2832adf5
etchepared <etcheparede@gmail.com>
2023-11-20 21:51:46 -0300

    feat: add completeTask functionality

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 //import axios from "axios";
 import { GetTasksList } from "../selectors/GetTasksList";
+import axios from "axios";
 
 export const TasksList = () => {
   const [tasks, setTasks] = useState([]);
@@ -11,6 +12,27 @@ export const TasksList = () => {
     });
   }, []);
 
+  async function onCheckboxClick(e) {
+    //buscar la task donde el id es igual al id del click y setear en true
+    let clickedTask = tasks.filter((t) => t.id == e.target.value);
+    const { id, tareaTitle, tareaDescription } = clickedTask[0];
+    clickedTask = {
+      tareaTitle: tareaTitle,
+      tareaDescription: tareaDescription,
+      isCompleted: true,
+    };
+
+    try {
+      await axios.put(
+        `http://localhost:5248/api/Tareas/UpdateTarea?id=${id}`,
+        clickedTask
+      );
+      location.reload();
+    } catch (error) {
+      console.log("No se actualizó la tarea.");
+    }
+  }
+
   return (
     <div>
       TasksList:
@@ -18,7 +40,11 @@ export const TasksList = () => {
         tasks.map((task) => {
           return (
             <div className="tarea" key={task.id}>
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                value={task.id}
+                onClick={onCheckboxClick}
+              />
               {task.tareaTitle}
             </div>
           );
